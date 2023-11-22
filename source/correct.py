16abd8e4696512968d39f090e9bd355ca202a451
Ameliouse <sun_amelie@hotmail.com>
2023-11-22 15:45:43 +0100

    ok

--- source/correct.py
+++ source/correct.py
@@ -1,30 +1,30 @@
 from scipy.optimize import minimize
 import numpy as np
 
-def unconditional_discounting(Alpha, Beta, K):
+def unconditional_discounting(decomp, Alpha, Beta):
+
+    J = len(decomp)
 
     def f_obj(e):
-        return e.sum()
+        return sum(e)
     
     def contraintes(e, p, alpha, beta):
 
-        contraintes_e = [1 - e_ij for e_ij in e]
-        contraintes_p = [1 - p_i for p_i in p]
-        contraintes_alpha = [((1-alpha[i][j])*p[i] - alpha[i][j]*p[j] - e[i][j]*(-alpha[i][j])) for i in range(K) for j in range(i+1,K)]
-        contraintes_beta = [((beta[i][j]-1)*p[i] + beta[i][j]*p[j] - e[i][j]*(beta[i][j]-1)) for i in range(K) for j in range(i+1,K)]
+        contraintes_e = [1 - e_j for e_j in e]
+        contraintes_p = [1 - p_k for p_k in p]
+        contraintes_alpha = [((1-alpha[j])*sum([p[i] for i in decomp[j][0]]) - alpha[j]*sum([p[i] for i in decomp[j][1]]) - e[j]*(-alpha[j])) for j in range(J)]
+        contraintes_beta = [((beta[j]-1)*sum([p[i] for i in decomp[j][0]]) + beta[j]*sum([p[i] for i in decomp[j][1]]) - e[j]*(beta[j]-1)) for j in range(J)]
 
         return contraintes_e + contraintes_p + contraintes_alpha + contraintes_beta
-    
-    alpha = Alpha
-    beta = Beta
-    p0 = [0.2] * K
-    e0 = np.array([[0,0.1,0.1,0.1],[0,0,0.1,0.1],[0,0,0,0.1],[0]*4])
+
+    p0 = [1/J]*J
+    e0 = [1/J]*J
 
     c_eq = {'type': 'eq', 'fun': lambda p: sum(p) - 1}
-    c_ineq = {'type': 'ineq', 'fun': contraintes, 'args': (p0, alpha, beta)}
+    c_ineq = {'type': 'ineq', 'fun': contraintes, 'args': (p0, Alpha, Beta)}
 
     res = minimize(f_obj, e0, constraints=[c_eq, c_ineq])
-
+    print(res.message)
     print("Valeur minimale trouvée :", res.fun)
     print("Valeur de la variable à minimiser (e) :", res.x)
 
--- source/main_test.py
+++ source/main_test.py
@@ -4,29 +4,31 @@ from utils import generate_multiclass_data
 import decomp
 from correct import unconditional_discounting
 
-# Parameters for each class (mean, std)
-class_parameters = [
-    ([1, 2], [1, 0.5]),
-    ([4, 5], [1.25, 2]),
-    ([0.25, 0.25], [0.5, 0.5]),
-    ([-2, 1], [0.5, 1])
-]
+# # Parameters for each class (mean, std)
+# class_parameters = [
+#     ([1, 2], [1, 0.5]),
+#     ([4, 5], [1.25, 2]),
+#     ([0.25, 0.25], [0.5, 0.5]),
+#     ([-2, 1], [0.5, 1])
+# ]
 
-# Génération de données synthétiques multiclasse
-num_samples_per_class = 100
-X, y = generate_multiclass_data(num_samples_per_class, class_parameters)
+# # Génération de données synthétiques multiclasse
+# num_samples_per_class = 100
+# X, y = generate_multiclass_data(num_samples_per_class, class_parameters)
 
-# Affichage graphique des données multiclasse
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap='viridis')
-plt.title('Multiclass Synthetic Data Distribution')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.show()
+# # Affichage graphique des données multiclasse
+# plt.scatter(X[:, 0], X[:, 1], c=y, cmap='viridis')
+# plt.title('Multiclass Synthetic Data Distribution')
+# plt.xlabel('Feature 1')
+# plt.ylabel('Feature 2')
+# plt.show()
 
-# Exemple de décomposition
-classifiers, decomps = decomp.ECOC_dense(X,y)
-print(decomps)
+# # Exemple de décomposition
+# classifiers, decomps = decomp.ECOC_dense(X,y)
+# print(decomps)
+# print(classifiers)
 
-# alpha = np.array([[0,0.6,0.6,0],[0,0,0.6,0.6],[0,0,0,0.6],[0]*4])
-# beta = np.array([[0,1,1,0,4],[0,0,1,1],[0,0,0,1],[0]*4])
-# correct.unconditional_discounting(alpha, beta, 4)
+decomp = [({0}, {1}), ({0}, {2}), ({0}, {3}), ({1}, {2}), ({1}, {3}), ({2}, {3})]
+alpha = [0.6,0.6,0,0.6,0.6,0.6]
+beta = [1,1,0.4,1,1,1]
+unconditional_discounting(decomp, alpha, beta)
